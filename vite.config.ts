7da1591b32eefdc78f6a48d5e1bd69705f61a39c
buddhin1949 <buddhin1949@gmail.com>
2026-07-29 16:43:21 +0530

    supa base references

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import viteTsConfigPaths from "vite-tsconfig-paths";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   plugins: [
@@ -12,5 +13,6 @@ export default defineConfig({
     tailwindcss(),
     tanstackStart(),
     viteReact(),
+    nitro({ preset: "vercel" }),
   ],
 });
